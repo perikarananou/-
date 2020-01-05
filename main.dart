ee91b51b871e3d67afb9f6eb6a079367c8c8c5e2
Karananou Peristera <56342189+perikrananou@users.noreply.github.com>
2020-01-05 21:43:01 +0200

    Revert "Update main.dart"

--- main.dart
+++ main.dart
@@ -12,10 +12,10 @@ void main(){
   do{
     var ap = stdin.readLineSync();
     if ( ap != swsth ){
-      print('Έχεις άλλη μία προσπάθεια.');
+      print('exeis allh mia prospathia.');
       i++;
     }else{
-      print('Σωστα!!!');
+      print('Swsta!!!');
       f != f;
     }
   }
@@ -30,10 +30,10 @@ void main(){
   do{
     var ap = stdin.readLineSync();
     if ( ap != swsth ){
-      print('Έχεις άλλη μία προσπάθεια.');
+      print('exeis allh mia prospathia.');
       i++;
     }else{
-      print('Σωστά!!!');
+      print('Swsta!!!');
       f != f;
     }
   }while(f==true || i==2);
@@ -41,16 +41,16 @@ void main(){
 
   f = false;
   i=0;
-  print('Πόσα bit ισοδυναμούν με ενα byte?');
+  print('Πόσα bit ισοδθναμούν μεενα byte?');
   print('a)4 /n b) 8 /n c) 9 ');
   swsth ="b";
   do{
     var ap = stdin.readLineSync();
     if ( ap != swsth ){
-      print('Έχεις άλλη μία προσπάθεια.');
+      print('exeis allh mia prospathia.');
       i++;
     }else{
-      print('Σωστά!!!');
+      print('Swsta!!!');
       f != f;
     }
   }while(f==true || i==2);
@@ -59,17 +59,16 @@ void main(){
   f = false;
   i=0;
   print('Ποιό από τα παρακάτω είναι συσκευή εισόδου?');
-  print('a)Σαρωτής /n b) Ηχείο /n c) Εκτυπωτής ');
+  print('a)Σρωτής /n b) Ηχείο /n c) Εκτυποτής ');
   swsth ="a";
   do{
     var ap = stdin.readLineSync();
     if ( ap != swsth ){
-      print('Έχεις άλλη μία προσπάθεια.');
+      print('exeis allh mia prospathia.');
       i++;
     }else{
-      print('Σωστά!!!');
+      print('Swsta!!!');
       f != f;
     }
   }while(f==true || i==2);
 }
-
