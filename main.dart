3d003a9cc87a52970eed4f68287f7219da5ac48b
Karananou Peristera <56342189+perikrananou@users.noreply.github.com>
2020-01-05 20:26:07 +0200

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+void main(){
+  bool f = false;
+  var swsth ="a";
+
+
+  print('Η οθόνη του υπολογιστή τι συσκευή είναι?');
+  print('a) εισόδου /n b) εξόδου /n c) ενδιάμεση');
+  swsth ="b";
+  var i=0;
+  do{
+    var ap = stdin.readLineSync();
+    if ( ap != swsth ){
+      print('exeis allh mia prospathia.');
+      i++;
+    }else{
+      print('Swsta!!!');
+      f != f;
+    }
+  }
+  while (f==true || i==2);
+
+
+  f = false;
+  i=0;
+  print('Τι είναι τα windows?');
+  print('a)υλικό(hardware) /n b) πόρτα /n c) λειτουργικό σύστημα');
+  swsth ="c";
+  do{
+    var ap = stdin.readLineSync();
+    if ( ap != swsth ){
+      print('exeis allh mia prospathia.');
+      i++;
+    }else{
+      print('Swsta!!!');
+      f != f;
+    }
+  }while(f==true || i==2);
+
+
+  f = false;
+  i=0;
+  print('Πόσα bit ισοδθναμούν μεενα byte?');
+  print('a)4 /n b) 8 /n c) 9 ');
+  swsth ="b";
+  do{
+    var ap = stdin.readLineSync();
+    if ( ap != swsth ){
+      print('exeis allh mia prospathia.');
+      i++;
+    }else{
+      print('Swsta!!!');
+      f != f;
+    }
+  }while(f==true || i==2);
+
+  
+  f = false;
+  i=0;
+  print('Ποιό από τα παρακάτω είναι συσκευή εισόδου?');
+  print('a)Σρωτής /n b) Ηχείο /n c) Εκτυποτής ');
+  swsth ="a";
+  do{
+    var ap = stdin.readLineSync();
+    if ( ap != swsth ){
+      print('exeis allh mia prospathia.');
+      i++;
+    }else{
+      print('Swsta!!!');
+      f != f;
+    }
+  }while(f==true || i==2);
+}
